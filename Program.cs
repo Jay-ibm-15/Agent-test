109c469c0e7d43c6af22a0646bbd68d46f4419bc
Jayaseelan Vellaisamy <vellaisamy.jayaseelan@ibm.com>
2025-10-31 14:43:25 +0530

    Clean workspace and add working Program.cs for SQL Server TblAgent access

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+	static string connectionString = @"Data Source=IBM-2J3XNC4\SQLEXPRESS;Initial Catalog=AgentTest;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;";
+
+	static void Main()
+	{
+		// Retrieve all agents
+		using (SqlConnection conn = new SqlConnection(connectionString))
+		{
+			conn.Open();
+			string selectQuery = "SELECT Id, AgentName FROM TblAgent";
+			using (SqlCommand cmd = new SqlCommand(selectQuery, conn))
+			using (SqlDataReader reader = cmd.ExecuteReader())
+			{
+				Console.WriteLine("Agents:");
+				while (reader.Read())
+				{
+					Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["AgentName"]}");
+				}
+			}
+		}
+	}
+}
